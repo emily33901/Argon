73292a9f37d01bf3681af864760308012464eaa1
Emily Hudson <emily33901@gmail.com>
2018-05-04 22:51:29 +0100

    Remove debug code

--- src/Testbed/DLLExport/DllExport.cs
+++ src/Testbed/DLLExport/DllExport.cs
@@ -14,16 +14,7 @@ namespace DllExportTest
         [DllExport(CallingConvention = CallingConvention.StdCall, ExportName = "CreateInterface")]
         public static IntPtr CreateInterface(IntPtr name)
         {
-            Console.WriteLine("CreateInterface called! name is {0}", name.ToString());
-
             var string_name = Marshal.PtrToStringAnsi(name);
-
-            if (string_name == null)
-            {
-                Console.WriteLine("string_name == null !");
-                return IntPtr.Zero;
-            }
-
             return User.CreateInterfaceNoUser(string_name);
         }
     }
