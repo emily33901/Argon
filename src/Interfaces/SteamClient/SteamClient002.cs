2f1e7d34e5c8516a2d053e1365e5bc0f92fab3d8
Emily Hudson <emily33901@gmail.com>
2018-05-05 13:56:20 +0100

    Refactor generator code to generate to files, add additional test interface

---
+++ src/Interfaces/SteamClient/SteamClient002.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSteamClient
+{
+    [ArgonCore.InterfaceImpl(Implements = "STEAMCLIENT_INTERFACE_VERSION", Name = "STEAMCLIENT_INTERFACE_VERSION002")]
+    public class SteamClient002 : ArgonCore.IBaseInterface
+    {
+        public int UserId { get; set; }
+
+        public int Test3(string a, int n)
+        {
+            return a.Length + n;
+        }
+
+        public string Test4(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
