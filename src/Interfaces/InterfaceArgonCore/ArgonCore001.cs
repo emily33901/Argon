e4d1d02c2f63b641d177e0bfe97026048e1b2cc9
Emily Hudson <emily33901@gmail.com>
2018-05-08 23:13:10 +0100

    Add ArgonCore interface

---
+++ src/Interfaces/InterfaceArgonCore/ArgonCore001.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ArgonCore;
+using ArgonCore.Interface;
+
+namespace InterfaceArgonCore
+{
+    /// <summary>
+    /// Exports Argon functions to unmanaged code
+    /// </summary>
+    [Impl(Name = "ArgonCore001", Implements = "ArgonCore")]
+    public class ArgonCore001 : IBaseInterface
+    {
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Linked to <see cref="User.CreateInterfaceNoUser(string)"/>
+        /// </summary>
+        /// <param name="name"></param>
+        public IntPtr CreateInterface(string name)
+        {
+            return User.CreateInterfaceNoUser(name);
+        }
+    }
+}
