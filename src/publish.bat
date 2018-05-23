00853515052480fe24ed10bb803ee6065214c303
Emily Hudson <emily33901@gmail.com>
2018-05-23 23:02:22 +0100

    Update publish.bat

--- src/publish.bat
+++ src/publish.bat
@@ -1,10 +1,8 @@
 @echo off
 
-pushd "%~dp0\Testbed\PublishTest"
-	dotnet publish --output "..\..\bin\Argon" --self-contained --framework netcoreapp2.0 --runtime win10-x86
-popd
-
 pushd "%~dp0"
+	dotnet publish Testbed/PublishTest/PublishHelper.csproj --output "..\..\bin\argon" --self-contained --framework netcoreapp2.0 --runtime win-x86
+
 	copy "cxx\bin\debug\testbed.exe" "bin\cpptestbed.exe"
 	copy "cxx\bin\debug\steamclient.dll" "bin\steamclient.dll"
 popd
