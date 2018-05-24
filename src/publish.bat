13c6168c05f614564bbc0e3ca0fc087528bd65f0
Emily Hudson <emily33901@gmail.com>
2018-05-24 14:15:08 +0100

    Update publish.bat to build cxx first

--- src/publish.bat
+++ src/publish.bat
@@ -1,6 +1,8 @@
 @echo off
 
 pushd "%~dp0"
+	call "cxx\make.bat"
+
 	dotnet publish Testbed/PublishTest/PublishHelper.csproj --output "..\..\bin\argon" --self-contained --framework netcoreapp2.0 --runtime win-x86
 
 	copy "cxx\bin\debug\testbed.exe" "bin\cpptestbed.exe"
