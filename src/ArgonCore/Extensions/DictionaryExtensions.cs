84bcf3b692938fd6934696209f29f7bbbd86edcc
Emily Hudson <emily33901@gmail.com>
2018-06-02 12:42:47 +0100

    Add FindOrCreate extension method

---
+++ src/ArgonCore/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace ArgonCore
+{
+    public static class DictionaryExtensions
+    {
+        public static Value FindOrCreate<Key, Value>(this IDictionary<Key, Value> dictionary, Key key)
+        where Value : new()
+        {
+            if (dictionary.TryGetValue(key, out var v)) return v;
+
+            v = new Value();
+            return (dictionary[key] = v);
+        }
+    }
+}
