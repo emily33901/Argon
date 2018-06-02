d3e0642e43b6f762f10a553b090aac6b608e97a8
Emily Hudson <emily33901@gmail.com>
2018-06-02 12:43:19 +0100

    Fully implement SteamFriends001

--- src/Interfaces/Implementation/Friends/Friends.cs
+++ src/Interfaces/Implementation/Friends/Friends.cs
@@ -1,15 +1,106 @@
 using System;
 
+using System.Collections.Generic;
+
 using ArgonCore;
 using ArgonCore.Interface;
 
+using SteamKit2;
+
 namespace InterfaceFriends
 {
+    class ChatMessage
+    {
+        public string Message { get; set; }
+        public SteamID Sender { get; set; }
+        public EChatEntryType Type { get; set; }
+    }
+
+    class ChatRoom
+    {
+        public static Dictionary<SteamID, ChatRoom> Active { get; set; } = new Dictionary<SteamID, ChatRoom>();
+        public List<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+    }
+
     class Friends : ClientTied<Friends>
     {
+        SteamFriends steam_friends;
+
         public Friends()
         {
+            steam_friends = Instance.SteamClient.GetHandler<SteamFriends>();
+            Instance.CallbackManager.Subscribe<SteamFriends.ChatMsgCallback>(cb => OnChatMessage(cb));
+        }
 
-        } 
+        public string GetLocalName() => steam_friends.GetPersonaName();
+        public EPersonaState GetLocalState() => steam_friends.GetPersonaState();
+        public void SetLocalState(EPersonaState state) => steam_friends.SetPersonaState(state);
+
+        public int GetFriendCount() => steam_friends.GetFriendCount();
+        public SteamID GetFriendByIndex(int index) => steam_friends.GetFriendByIndex(index);
+        public bool HasFriend(SteamID id)
+        {
+            var max = steam_friends.GetFriendCount();
+            for (int i = 0; i < max; i++)
+            {
+                if (steam_friends.GetFriendByIndex(i) == id) return true;
+            }
+
+            return false;
+        }
+
+        public void AddFriend(SteamID s) => steam_friends.AddFriend(s);
+        public void AddFriend(string s) => steam_friends.AddFriend(s);
+        public void RemoveFriend(SteamID s) => steam_friends.RemoveFriend(s);
+        public EFriendRelationship GetRelationship(SteamID s) => steam_friends.GetFriendRelationship(s);
+        public EPersonaState GetFriendState(SteamID s) => steam_friends.GetFriendPersonaState(s);
+        public GameID GamePlayed(SteamID s) => steam_friends.GetFriendGamePlayed(s);
+        public string GetName(SteamID s) => steam_friends.GetFriendPersonaName(s);
+
+        public void SendMsgToFriend(SteamID target, EChatEntryType type, string message)
+        {
+            steam_friends.SendChatMessage(target, type, message);
+        }
+
+        public string GetFriendNameHistory(SteamID s, int index)
+        {
+            return "";
+        }
+
+        public int GetBlockedFriendCount()
+        {
+            var total = 0;
+            var max = steam_friends.GetFriendCount();
+            for (int i = 0; i < max; i++)
+            {
+                var friend_id = steam_friends.GetFriendByIndex(i);
+                if (steam_friends.GetFriendRelationship(friend_id) == EFriendRelationship.Blocked)
+                    total += 1;
+            }
+
+            return total;
+        }
+
+        void OnChatMessage(SteamFriends.ChatMsgCallback cb)
+        {
+            Log.WriteLine("Chat message recieved...");
+
+            var room_id = cb.ChatRoomID;
+
+            var room = ChatRoom.Active.FindOrCreate(room_id);
+
+            room.Messages.Add(new ChatMessage { Message = cb.Message, Sender = cb.ChatterID, Type = cb.ChatMsgType });
+        }
+
+        public ChatMessage GetChatMessage(SteamID s, int id)
+        {
+            if (ChatRoom.Active.TryGetValue(s, out var r))
+            {
+                if (id > r.Messages.Count - 1) return null;
+                return r.Messages[id];
+            }
+
+            return null;
+        }
     }
 }
--- src/Interfaces/Implementation/Friends/SteamFriends001.cs
+++ src/Interfaces/Implementation/Friends/SteamFriends001.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using System.Runtime.InteropServices;
+
 using ArgonCore;
 using ArgonCore.Interface;
 
@@ -17,7 +19,167 @@ namespace InterfaceFriends
 
         public string GetPersonaName()
         {
+            return f.GetLocalName();
+        }
+
+        public uint GetPersonaState()
+        {
+            return (uint)f.GetLocalState();
+        }
+
+        public void SetPersonaState(uint state)
+        {
+            f.SetLocalState((SteamKit2.EPersonaState)state);
+        }
+
+        public bool AddFriend(ulong steam_id)
+        {
+            f.AddFriend(new SteamKit2.SteamID(steam_id));
+
+            return true;
+        }
+
+        public bool RemoveFriend(ulong steam_id)
+        {
+            f.RemoveFriend(new SteamKit2.SteamID(steam_id));
+
+            return false;
+        }
+
+        public bool HasFriend(ulong steam_id)
+        {
+            return f.HasFriend(new SteamKit2.SteamID(steam_id));
+        }
+
+        public uint GetFriendRelationship(ulong steam_id)
+        {
+            return (uint)f.GetRelationship(new SteamKit2.SteamID(steam_id));
+        }
+
+        public uint GetFriendPersonaState(ulong steam_id)
+        {
+            return (uint)f.GetFriendState(new SteamKit2.SteamID(steam_id));
+        }
+
+        public bool Deprecated_GetFriendGamePlayed(ulong steam_id, ref uint app_id, ref uint game_ip, ref uint game_port)
+        {
+            Console.WriteLine("Deprecated_GetFriendGamePlayed called!");
+
+            var game_id = f.GamePlayed(new SteamKit2.SteamID(steam_id));
+            app_id = game_id.AppID;
+
+            return game_id.AppID != 0;
+        }
+
+        public string GetFriendPersonaName(ulong steam_id)
+        {
+            return f.GetName(new SteamKit2.SteamID(steam_id));
+        }
+
+        // TODO: this returns a HSteamCall, but that isnt a thing anymore??
+        // For now we just perform the action and then return 0
+        public int AddFriendByName(string name)
+        {
+            f.AddFriend(name);
+            return 0;
+        }
+
+        public int GetFriendCount()
+        {
+            return f.GetFriendCount();
+        }
+
+        public ulong GetFriendByIndex(int index)
+        {
+            return f.GetFriendByIndex(index);
+        }
+
+        public void SendMsgToFriend(ulong dest_steam_id, uint msg_type, string message_body)
+        {
+            var steam_id = new SteamKit2.SteamID(dest_steam_id);
+
+            f.SendMsgToFriend(steam_id, (SteamKit2.EChatEntryType)msg_type, message_body);
+        }
+
+        public void SetFriendRegValue(ulong steam_id, string key, string value)
+        {
+            // TODO: hook up to configstore
+        }
+
+        public string GetFriendRegValue(ulong steam_id, string key)
+        {
+            // TODO: hook up to configstore
             return "";
         }
+
+        public string GetFriendPersonaNameHistory(ulong steam_id, int index)
+        {
+            return "";
+        }
+
+        public int GetChatMessage(ulong steam_id, int msg_index, IntPtr msg_out, int max_msg, ref uint msg_type)
+        {
+            var id = new SteamKit2.SteamID(steam_id);
+            var cm = f.GetChatMessage(id, msg_index);
+
+            if (cm != null)
+            {
+                var total_wrote = Math.Max(max_msg, cm.Message.Length);
+                Marshal.Copy(System.Text.Encoding.ASCII.GetBytes(cm.Message), 0, msg_out, max_msg);
+                msg_type = (uint)cm.Type;
+
+                return total_wrote;
+            }
+
+            return 0;
+        }
+
+        public bool SendMsgToFriend(ulong steam_id, uint type, string message, int length)
+        {
+            f.SendMsgToFriend(new SteamKit2.SteamID(steam_id), (SteamKit2.EChatEntryType)type, message);
+
+            return true;
+        }
+
+        public int GetChatIDOfChatHistoryStart(ulong steam_id)
+        {
+            Console.WriteLine("GetChatIDOfChatHistoryStart should never be called!");
+            return 0;
+        }
+
+        public void SetChatHistoryStart(ulong steam_id, int index)
+        {
+            Console.WriteLine("SetChatHistoryStart should never be called!");
+        }
+
+        public void ClearChatHistory(ulong steam_id)
+        {
+        }
+
+        public int InviteFriendByEmail(string email)
+        {
+            f.AddFriend(email);
+
+            return 0;
+        }
+
+        public int GetBlockedFriendCount()
+        {
+            return f.GetBlockedFriendCount();
+        }
+
+        public bool GetFriendGamePlayed(ulong steam_id, ref ulong game_id, ref uint server_ip, ref ushort server_port)
+        {
+            var id = new SteamKit2.SteamID(steam_id);
+
+            game_id = f.GamePlayed(id);
+
+            return true;
+        }
+        public bool GetFriendGamePlayed2(ulong steam_id, ref ulong game_id, ref uint server_ip, ref ushort server_port)
+        {
+            return GetFriendGamePlayed(steam_id, ref game_id, ref server_ip, ref server_port);
+        }
+
     }
 }
