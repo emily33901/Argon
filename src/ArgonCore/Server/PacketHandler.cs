bc3847e2e8f91f98fc06ad150105856a5dc999b4
Emily Hudson <emily33901@gmail.com>
2018-06-09 10:25:56 +0100

    Update packethandler to check for asynccalls and deal with the appropriately

--- src/ArgonCore/Server/PacketHandler.cs
+++ src/ArgonCore/Server/PacketHandler.cs
@@ -22,7 +22,7 @@ namespace ArgonCore.Server
 
         public override void HandleMsg(IPacketMsg packet)
         {
-            Log.WriteLine("{0} [{1}]", packet.MsgType, (uint)packet.MsgType);
+            Log.WriteLine("{0} [{1}] [{2]}] -> [{3}]", packet.MsgType, (uint)packet.MsgType, packet.SourceJobID, packet.TargetJobID);
 
             Server.Client.PendingCallbacks.Enqueue(new ArgonCore.Client.InternalCallbackMsg
             {
@@ -30,6 +30,29 @@ namespace ArgonCore.Server
                 callback_id = (uint)packet.MsgType,
                 data = packet.GetData(),
             });
+
+            var job_result = AsyncCallManager.GetAsyncJob(packet.TargetJobID);
+
+            if (job_result != null)
+            {
+                Log.WriteLine("... Was AsyncCall, queuing callback now!");
+
+                job_result.finished = true;
+
+                var result_buffer = new ArgonCore.Util.Buffer();
+                result_buffer.Write(job_result.job_id);
+                result_buffer.Write(job_result.callback_id);
+                result_buffer.Write(packet.GetData().Length);
+                result_buffer.Write(packet.GetData());
+
+                // TODO: Post callback to clients
+                Server.Client.PendingCallbacks.Enqueue(new ArgonCore.Client.InternalCallbackMsg
+                {
+                    callback_id = 703,
+                    user_id = -1,
+                    data = result_buffer.GetBuffer(),
+                });
+            }
         }
     }
 }
