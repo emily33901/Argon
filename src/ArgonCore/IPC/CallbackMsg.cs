29945802cf35368c4be6dd1a40f4ae9ab8e7df8f
Emily Hudson <emily33901@gmail.com>
2018-07-14 11:30:43 +0100

    Move callbackmsg into ipc from client

--- src/ArgonCore/IPC/CallbackMsg.cs
+++ src/ArgonCore/IPC/CallbackMsg.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
 
-namespace ArgonCore.Client
+namespace ArgonCore.IPC
 {
     [Serializable]
     public class InternalCallbackMsg
