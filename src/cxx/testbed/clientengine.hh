a40fc5c39523f1af10e62a8c85f091ab62e06fcb
Emily Hudson <emily33901@gmail.com>
2018-07-18 21:32:25 +0100

    Add a login to the testbed

---
+++ src/cxx/testbed/clientengine.hh
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "../steam/steam_api.h"
+
+using IConCommandBaseAccessor = void *;
+
+class IClientEngine005 {
+public:
+    virtual HSteamPipe CreateSteamPipe()                                                       = 0;
+    virtual bool       BReleaseSteamPipe(HSteamPipe)                                           = 0;
+    virtual int        CreateGlobalUser(HSteamPipe *)                                          = 0;
+    virtual HSteamUser ConnectToGlobalUser(HSteamPipe)                                         = 0;
+    virtual HSteamUser CreateLocalUser(HSteamPipe *, EAccountType)                             = 0;
+    virtual int        CreatePipeToLocalUser(int, int *)                                       = 0;
+    virtual int        ReleaseUser(int, int)                                                   = 0;
+    virtual int        IsValidHSteamUserPipe(int, int)                                         = 0;
+    virtual void *     GetIClientUser(int, int, char const *)                                  = 0;
+    virtual int        GetIClientGameServer(int, int, char const *)                            = 0;
+    virtual int        SetLocalIPBinding(unsigned int, unsigned short)                         = 0;
+    virtual int        GetUniverseName(EUniverse)                                              = 0;
+    virtual int        GetIClientFriends(int, int, char const *)                               = 0;
+    virtual int        GetIClientUtils(int, char const *)                                      = 0;
+    virtual int        GetIClientBilling(int, int, char const *)                               = 0;
+    virtual int        GetIClientMatchmaking(int, int, char const *)                           = 0;
+    virtual int        GetIClientApps(int, int, char const *)                                  = 0;
+    virtual int        GetIClientMatchmakingServers(int, int, char const *)                    = 0;
+    virtual int        GetIClientGameSearch(int, int, char const *)                            = 0;
+    virtual int        RunFrame()                                                              = 0;
+    virtual int        GetIPCCallCount()                                                       = 0;
+    virtual int        GetIClientUserStats(int, int, char const *)                             = 0;
+    virtual int        GetIClientGameServerStats(int, int, char const *)                       = 0;
+    virtual int        GetIClientNetworking(int, int, char const *)                            = 0;
+    virtual int        GetIClientRemoteStorage(int, int, char const *)                         = 0;
+    virtual int        GetIClientScreenshots(int, int, char const *)                           = 0;
+    virtual int        SetWarningMessageHook(void (*)(int, char const *))                      = 0;
+    virtual int        GetIClientGameCoordinator(int, int, char const *)                       = 0;
+    virtual int        SetOverlayNotificationPosition(ENotificationPosition)                   = 0;
+    virtual int        SetOverlayNotificationInset(int, int)                                   = 0;
+    virtual int        HookScreenshots(bool)                                                   = 0;
+    virtual int        IsScreenshotsHooked()                                                   = 0;
+    virtual int        IsOverlayEnabled()                                                      = 0;
+    virtual int        GetAPICallResult(int, unsigned long long, void *, int, int, bool *)     = 0;
+    virtual int        GetIClientProductBuilder(int, int, char const *)                        = 0;
+    virtual int        GetIClientDepotBuilder(int, int, char const *)                          = 0;
+    virtual int        GetIClientNetworkDeviceManager(int, char const *)                       = 0;
+    virtual int        ConCommandInit(IConCommandBaseAccessor *)                               = 0;
+    virtual int        GetIClientAppManager(int, int, char const *)                            = 0;
+    virtual int        GetIClientConfigStore(int, int, char const *)                           = 0;
+    virtual int        BOverlayNeedsPresent()                                                  = 0;
+    virtual int        GetIClientGameStats(int, int, char const *)                             = 0;
+    virtual int        GetIClientHTTP(int, int, char const *)                                  = 0;
+    virtual int        BShutdownIfAllPipesClosed()                                             = 0;
+    virtual int        GetIClientAudio(int, int, char const *)                                 = 0;
+    virtual int        GetIClientMusic(int, int, char const *)                                 = 0;
+    virtual int        GetIClientUnifiedMessages(int, int, char const *)                       = 0;
+    virtual int        GetIClientController(int, int, char const *)                            = 0;
+    virtual int        GetIClientParentalSettings(int, int, char const *)                      = 0;
+    virtual int        GetIClientStreamLauncher(int, int, char const *)                        = 0;
+    virtual int        GetIClientDeviceAuth(int, int, char const *)                            = 0;
+    virtual int        GetIClientRemoteClientManager(int, char const *)                        = 0;
+    virtual int        GetIClientStreamClient(int, int, char const *)                          = 0;
+    virtual int        GetIClientShortcuts(int, int, char const *)                             = 0;
+    virtual int        GetIClientUGC(int, int, char const *)                                   = 0;
+    virtual int        GetIClientInventory(int, int, char const *)                             = 0;
+    virtual int        GetIClientVR(int, char const *)                                         = 0;
+    virtual int        GetIClientGameNotifications(int, int, char const *)                     = 0;
+    virtual int        GetIClientHTMLSurface(int, int, char const *)                           = 0;
+    virtual int        GetIClientVideo(int, int, char const *)                                 = 0;
+    virtual int        GetIClientControllerSerialized(int, char const *)                       = 0;
+    virtual int        GetIClientAppDisableUpdate(int, int, char const *)                      = 0;
+    virtual int        Set_Client_API_CCheckCallbackRegisteredInProcess(unsigned int (*)(int)) = 0;
+    virtual int        GetIClientBluetoothManager(int, char const *)                           = 0;
+    virtual int        GetIClientSharedConnection(int, int, char const *)                      = 0;
+    virtual int        GetIClientShader(int, int, char const *)                                = 0;
+    virtual int        GetIClientNetworkingSocketsSerialized(int, int, char const *)           = 0;
+    virtual int        destructor1()                                                           = 0;
+    virtual int        GetIPCServerMap()                                                       = 0;
+    virtual int        OnDebugTextArrived(char const *)                                        = 0;
+    virtual int        OnThreadLocalRegistration()                                             = 0;
+    virtual int        OnThreadBuffersOverLimit()                                              = 0;
+};
--- src/cxx/testbed/testbed.cc
+++ src/cxx/testbed/testbed.cc
@@ -11,6 +11,9 @@ using DWORD = unsigned int;
 
 #include "../steam/steam_api.h"
 
+#include "clientengine.hh"
+#include "clientuser.hh"
+
 using CreateInterfaceFn = void *(*)(const char *);
 using GetCallbackFn     = bool (*)(unsigned int pipe, CallbackMsg_t *callback);
 using FreeCallbackFn    = bool (*)(unsigned int pipe);
@@ -46,17 +49,40 @@ int main(int argc, const char **argv) {
 #endif
 
     if (create_interface_proc != nullptr) {
-        auto steam_client = (ISteamClient017 *)create_interface_proc("SteamClient017");
+        auto client_engine = (IClientEngine005 *)create_interface_proc("CLIENTENGINE_INTERFACE_VERSION005");
+        auto steam_client  = (ISteamClient017 *)create_interface_proc("SteamClient017");
 
         if (steam_client != nullptr) {
-            auto pipe_handle = steam_client->CreateSteamPipe();
-            auto user_handle = steam_client->CreateLocalUser(&pipe_handle, k_EAccountTypeIndividual);
-            auto user        = steam_client->GetISteamUser(user_handle, pipe_handle, "SteamUser019");
+            HSteamPipe pipe_handle = client_engine->CreateSteamPipe();
+            auto       user_handle = client_engine->CreateLocalUser(&pipe_handle, k_EAccountTypeIndividual);
+            auto       user        = (IClientUser001 *)client_engine->GetIClientUser(user_handle, pipe_handle, "CLIENTUSER_INTERFACE_VERSION001");
 
-            if (user == nullptr) printf("Unable to get user!\n");
+            if (user == nullptr) {
+                printf("Unable to get user!\n");
+                return 1;
+            }
+
+            char username[128];
+            char password[128];
+
+            printf("Enter your username: ");
+            scanf("%s", username);
+
+            printf("Enter your password: ");
+            scanf("%s", password);
+
+            user->LogOnWithPassword(username, password);
+
+            char twofactor[128];
+
+            printf("Enter 2fa: ");
+            scanf("%s", twofactor);
+
+            user->SetTwoFactorCode(twofactor);
+
+            user->LogOn({});
 
             CallbackMsg_t msg;
-
             while (true) {
                 while (get_next_callback(pipe_handle, &msg)) {
                     printf("msg from user %u: id: %d size: %u\n", msg.m_hSteamUser, msg.m_iCallback, msg.m_cubParam);
