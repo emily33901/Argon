743dc8083079c4b8a8ea3e4aaf43610b7f95e98d
F1ssi0N <Josh33901@gmail.com>
2018-08-06 19:53:51 +0100

    Add test for buffer attributes

---
+++ src/Interfaces/Implementation/ServerMappedTest/MappedTest001.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using ArgonCore.Interface;
+using ArgonCore.IPC;
+
+namespace InterfaceArgonCore
+{
+    /// <summary>
+    /// Series of tests for the mapping code
+    /// </summary>
+    [Impl(Name = "MappedTest001", ServerMapped = true)]
+    public class MappedTest001 : IBaseInterface
+    {
+        public int PointerTest(ref int a, ref int b, ref int c)
+        {
+            c = a + b;
+            b = 5;
+            a = 4;
+            return a + b + c;
+        }
+
+        [Buffer(Index = 0, NewPointerIndex = 0, NewSizeIndex = 1)]
+        public int BufferTest(ref ArgonCore.Util.Buffer b)
+        {
+            // Reset the buffer
+            b.Reset();
+
+            // Write raw chars here as that is what the test expects
+            // Writing typeof(string) will write the length of the string first
+            // (which is useful for deserialization but isnt what this is testing for)
+            b.Write("13 characters".ToCharArray());
+
+            return b.Length();
+        }
+    }
+}
